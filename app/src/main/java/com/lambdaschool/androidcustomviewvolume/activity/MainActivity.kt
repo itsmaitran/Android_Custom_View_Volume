596ebd7c16b22b6867b82ec4f84510dba417074f
itsmaisn <itsmaisn@gmail.com>
2019-09-03 22:06:36 -0700

    Assignment complete.

---
+++ app/src/main/java/com/lambdaschool/androidcustomviewvolume/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.lambdaschool.androidcustomviewvolume.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lambdaschool.androidcustomviewvolume.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
